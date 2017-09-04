a85ee8683973606f5be15e557f1ee70c84740ff2
chenb <353549444@qq.com>
2017-09-04 09:16:14 +0800

    修改run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#删除以前的jenkins目录
-rm -rf /var/jenkins_home
+#删除以前的jenkins目录（慎重）
+# rm -rf /var/jenkins_home
 
 #新建新的目录
-mkdir /var/jenkins_home
+# mkdir /var/jenkins_home
 
 #给新建的目录添加权限
-chmod 777 /var/jenkins_home
+# chmod 777 /var/jenkins_home
 
 #运行jenkins容器，将宿主的 /var/run/docker.sock 文件挂载到容器内的同样位置，从而让容器内可以通过 unix socket 调用宿主的 Docker 引擎。
 #将jenkins目录挂载到宿主机同样位置，以便查看初始密码和workspace
